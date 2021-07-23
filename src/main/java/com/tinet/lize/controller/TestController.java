c9f9f8c4fa2b450dd4108e7527307f9d0b327e5f
lize <lize@ti-net.com.cn>
2021-07-23 13:59:13 +0800

    测试controller  测试html

---
+++ src/main/java/com/tinet/lize/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.tinet.lize.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lize
+ * @date 2021年07月23日 11:33
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test1122";
+    }
+}
