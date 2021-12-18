d13e5ffcdbda28668cedf3fa021a781be707ce62
lize <lizien_ywxk@163.com>
2021-12-18 12:18:47 +0800

    111

--- src/main/java/com/tinet/lize/vo/SizeVo.java
+++ src/main/java/com/tinet/lize/vo/SizeVo.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  * @author lize
@@ -16,4 +17,8 @@ public class SizeVo {
     @NotNull
     @Length(max = 5,message = "最大长度为5")
     private String name;
+
+    @NotNull
+    @Pattern(regexp = "^(http|https)://([\\w.]+\\/?)\\S*",message = "url格式错误")
+    private String url;
 }
